ddca274e0646914efff91e1c5b0abc52708507c9
Shaw <michele_aliffi@yahoo.it>
2021-11-25 15:13:17 +0100

    Aggiunto quasi tutto, manca solo rendere codice e prezzo positivi

--- src/it/shop/Main.java
+++ src/it/shop/Main.java
@@ -3,8 +3,11 @@ package it.shop;
 public class Main {
 
 	public static void main(String[] args) {
-		
-
+		Prodotto prodotto1 = new Prodotto();
+		System.out.println("Prodotto " + prodotto1.getId());
+		System.out.println("Prezzo Iva esclusa " + prodotto1.getPrezzo() + "€");
+		System.out.println("Prezzo Iva inclusa " + prodotto1.prezzoIncluso() + "€");
+		System.out.println("Codice prodotto: " + prodotto1.getId() + prodotto1.getNome());
 	}
 
 }
--- src/it/shop/Prodotto.java
+++ src/it/shop/Prodotto.java
@@ -1,9 +1,69 @@
 package it.shop;
 
+import java.util.Random;
+
 public class Prodotto {
-	int id;
-	String nome;
-	String descrizione;
-	double prezzo;
-	double iva;
+	private int id;
+	private String nome;
+	private String descrizione;
+	private float prezzo;
+	private float iva = (float)0.22;
+	
+	public Prodotto() {
+		this.id = generateId();
+		this.prezzo = generatePrezzo();
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescrizione() {
+		return descrizione;
+	}
+
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+
+	public float getPrezzo() {
+		return prezzo;
+	}
+
+	public void setPrezzo(float prezzo) {
+		this.prezzo = prezzo;
+	}
+
+	public float getIva() {
+		return iva;
+	}
+
+	public void setIva(float iva) {
+		this.iva = iva;
+	}
+
+	public int getId() {
+		return id;
+	}
+	
+	
+	public int generateId() {
+		Random randomGen = new Random();
+		return randomGen.nextInt();
+	}
+	
+	public float generatePrezzo() {
+		Random randomGen = new Random();
+		return randomGen.nextFloat();
+	}
+	
+	public float prezzoIncluso() {
+		float prezzoIncluso = getPrezzo() + generatePrezzo() * iva;
+		return prezzoIncluso;
+	}
+	
 }
