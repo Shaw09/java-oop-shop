5779f56436256dfc5c3bef9710c20081818d7ce7
Shaw <michele_aliffi@yahoo.it>
2021-11-25 14:09:43 +0100

    Aggiunti file

---
+++ src/it/shop/Main.java
@@ -0,0 +1,10 @@
+package it.shop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/it/shop/Prodotto.java
@@ -0,0 +1,9 @@
+package it.shop;
+
+public class Prodotto {
+	int id;
+	String nome;
+	String descrizione;
+	double prezzo;
+	double iva;
+}
